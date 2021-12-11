00532266e622ee77e52656f70042c927c30d3dbd
Mark_GitHub <95771108+lianzhiliang1@users.noreply.github.com>
2021-12-11 09:43:47 +0800

    Initialize variadic-pass

---
+++ docs/guide/creating-variadics/update-screenshots.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+"$SHOT" ./samples.xodball pin-assignments ./assignments.patch.png 400
+"$SHOT" ./samples.xodball join ./join.patch.png 400
+# ./join.patch.png
+# ./marker-error.patch.png
